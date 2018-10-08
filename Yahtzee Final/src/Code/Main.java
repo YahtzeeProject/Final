627c2bdc814315472a404ca927b4906b3697125f
Cookedchicken <34245870+Cookedchicken@users.noreply.github.com>
2018-10-08 09:52:27 -0500

    h

--- Yahtzee Final/src/Code/Main.java
+++ Yahtzee Final/src/Code/Main.java
@@ -1,10 +1,16 @@
 package Code;
 
+import.util.Scanner;
+
 public class Main {
 
+	System(Scanner.in);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+	
+		
 	}
 
 }
