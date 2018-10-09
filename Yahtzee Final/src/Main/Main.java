e5ae9105885da53ea001432058f4ffa535da3b6e
Cookedchicken <34245870+Cookedchicken@users.noreply.github.com>
2018-10-09 09:51:07 -0500

    started level 2

--- Yahtzee Final/src/Main/Main.java
+++ Yahtzee Final/src/Main/Main.java
@@ -1,11 +1,8 @@
-package Code;
+package Main;
 
-import.util.Scanner;
 
 public class Main {
 
-	System(Scanner.in);
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
--- Yahtzee Final/src/Main/level2.java
+++ Yahtzee Final/src/Main/level2.java
@@ -1,6 +1,6 @@
-package Code;
+package Main;
 
-public class Random {
+public class level2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
