da6ebd1e1882b44e5a8b1f4db32562496127a63b
Cookedchicken <34245870+Cookedchicken@users.noreply.github.com>
2018-10-10 09:48:41 -0500

    h

--- Yahtzee Final/src/Words/words2normal.java
+++ Yahtzee Final/src/Words/words2normal.java
@@ -1,10 +1,16 @@
 package Words;
 
 public class words2normal {
-
+	public static String[]words = new String [10];
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int random = (int)(Math.random());
+		int random = (int)(Math.random()* 20 + 2);
+		
+		
+		String answer;
+		String acrophobia = "A fear of spiders";
+		String archipelago = "A group of islands ";
+		String 
 		
 	}
 
