18a3bad51e501b305df05cc90f378d46d951fb35
Joey <grammerking113@gmail.com>
2018-10-10 09:15:38 -0500

    Up

--- Yahtzee Final/src/Levels/Level1.java
+++ Yahtzee Final/src/Levels/Level1.java
@@ -3,17 +3,16 @@ package Levels;
 import javax.swing.*;
 
 public class Level1 {
+	public static String[] words2 = new String[5];
 
 	public static void main(String[] args) {
 		
 		Words.Words1Easy.words();
 		
-		JFrame frame = new JFrame("Words");
+		JOptionPane.showMessageDialog(null,JOptionPane.showInputDialog(null,"Please enter a message"));
 		
-		String name = JOptionPane.showInputDialog(frame, );
-		
-		System.out.printf("The user's name is '%s'.\n", name);
-	    System.exit(0);		
+		String message =  JOptionPane.showInputDialog(null, [0]);
+		JOptionPane.showMessageDialog(null,message);
 		
 		
 		
