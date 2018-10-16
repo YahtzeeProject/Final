9f2d45f3dad5b67d56c08f4273a168fbb0f3e89f
Joey <grammerking113@gmail.com>
2018-10-16 09:06:08 -0500

    Update

--- Yahtzee Final/src/Levels/Level1.java
+++ Yahtzee Final/src/Levels/Level1.java
@@ -16,31 +16,8 @@ public class Level1 {
 		Words.words1easy.words();
 		String message;
 		
-		
-		
-		Object[] options = {"Party",
-                "Loop",
-                "Airplane",
-                "Balloon",
-                "Bee",
-                "Bomb",
-                "Chef",
-                "Drink",
-                "Eggs",
-                "Fan"};
-		
 		Scanner input  = new Scanner(System.in);
 
-				
-/*					int n = JOptionPane.showOptionDialog(null,//parent container of JOptionPane
-							Words.words1easy.words[0],
-					"Words Q1",
-					JOptionPane.YES_NO_CANCEL_OPTION,
-					JOptionPane.QUESTION_MESSAGE,
-					null,//do not use a custom Icon
-					options,//the titles of buttons
-					options[2]);//default button title
-*/					
 					System.out.println(Words.words1easy.ans[0]);
 					System.out.println(Words.words1easy.words[0]);
 
@@ -129,7 +106,7 @@ public class Level1 {
 							}
 					 }while(!Words.words1easy.ans[4].equals(message));
 
-
+					 message=JOptionPane.showInputDialog(null,"Would You like to play level 2?");
 	}
 	
 
--- Yahtzee Final/src/Words/words2normal.java
+++ Yahtzee Final/src/Words/words2normal.java
@@ -1,11 +1,22 @@
 package Words;
 
 public class words2normal {
+	
+	public static String[] words2 = new String[5];
 
+	public static String[] ans2 = new String[5];
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int random = (int)(Math.random());
+		String[] define = { "The fear of spiders!", "A group of islands.", "A mountain of ice", "A ball with helium.", "Bzzz Bzzzz.","Terrorists use this.","They cook food.","Something you do to liquids.","Chickens lay these." , "Blows air."};
 		
+		String Arachnophobia = "The fear of spiders";
+		String Archipelago = "A group of islands";
+		String Glacier = "A mountian of ice";
+		String Normies = "Degenarates";
+		String Xenophobia = "A dislkie of people froms other countries";
+		String Narcissism = "Valuing yourself above all others";
+		String Tertiphobia = "Fear of the number 4";
 	}
 
 }
